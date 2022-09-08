b84b3f49096e4951ad08878306c643be9e0711fb
Matheus Carvalho <matheus12bh@gmail.com>
2022-09-08 10:07:11 -0700

    first commit

---
+++ invidiual_assignment_1.py
@@ -0,0 +1,21 @@
+print('First year salary: ')
+salary = input()
+
+print('Enter the the time you have taught (in Years): ')
+years = input()
+salariesByYear= [];
+
+salariesByYear.append(
+  {
+    'year': 1,
+    'salary': round(int(salary),2)
+  }
+);
+
+for year in range(0, int(years)-1):
+  print(year)
+  data = {
+    'year': salariesByYear[year]['year'] + 1,
+    'salary': round((salariesByYear[year]['salary'] * 1.032),2) if year < 13 else round((salariesByYear[12]['salary'] * 1.032),2)
+  }
+  salariesByYear.append(data)
