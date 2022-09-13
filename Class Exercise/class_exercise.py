cce52173d4092d709ad046e56874ea806d6094c0
Matheus Carvalho <matheus12bh@gmail.com>
2022-09-13 15:18:20 -0700

    add-individual-assignment-code

--- Class Exercise/class_exercise.py
+++ Class Exercise/class_exercise.py
@@ -4,31 +4,32 @@ Created on Tue Sep 13 13:49:08 2022
 
 @author: Matheus Carvalho
 """
-def getBMILabel(BMI): 
-  if BMI <= 18.5:
+def getBMILabel(bmi): 
+  if bmi <= 18.5:
     return 'Under weight'
-  if BMI <= 24.9:
+  if bmi <= 24.9:
     return 'Normal'
-  if BMI <= 29.9:
+  if bmi <= 29.9:
     return 'Over Weight'
-  if BMI <= 34.9:
+  if bmi <= 34.9:
     return 'Obesity (Class I)'
-  if BMI <= 39.9:
+  if bmi <= 39.9:
     return 'Obesity (Class II)'
-  if BMI > 40:
+  if bmi > 40:
     return 'Extreme Obesity'
 
 print('BMI Calculator')
 
-print('Your Height (in inches): ')
-height = input()
+height = float(input('Your Height (in inches): '))
+weight = float(input('Your Weigth (in pounds): '))
 
-print('Your Weigth (in pounds): ')
-weight = input()
+if height <= 0 or weight <= 0:
+  print('Invalid input, please try again!')
+  exit()
+  
+heightConvertedToMeters = height / 39.37
+weightConvertedToKilograms = weight / 2.205
 
-heightConvertedToMeters = float(height) / 39.37
-weightConvertedToKilograms = float(weight) / 2.205
+bmi = weightConvertedToKilograms / pow(heightConvertedToMeters,2)
 
-BMI = weightConvertedToKilograms / pow(heightConvertedToMeters,2)
-
-print('Your IBM is ' + str(round(BMI,1)) + ' and your classification is ' + getBMILabel(BMI))
+print('Your BMI is ' + str(round(bmi,1)) + ' and your classification is ' + getBMILabel(bmi))
--- Individual Assignment 2/individual_assignment_2.py
+++ Individual Assignment 2/individual_assignment_2.py
@@ -4,3 +4,45 @@ Created on Tue Sep 13 13:52:43 2022
 
 @author: Matheus Carvalho
 """
+# Define constants to facilitate further updates
+UNLIMITED_CALLS_AND_TEXTS = 9.99
+OLD_CUSTOMER_RATE = 9.50
+NEW_CUSTOMER_RATE = 11.30
+
+# Set function to format floats and integers to Currency Format
+def formatCurrency(value):
+  return '$' + str(format(value,'.2f'))
+
+# Print initial message
+print('The purpose of the program is to calculate the cell phone bill for customers of a cell phone company')
+
+# Get data usage from user
+print('Please enter the data usage last month in GB:')
+dataUsage = float(input())
+
+# Get account year from user
+print('Please enter the year the account is opend:')
+accountYear = float(input())
+
+# Validate if the inputs are valid 
+if dataUsage <= 0 or accountYear <= 0:
+  print('Invalid input, please try again!')
+  exit()
+
+# Check if the user is an old or a new customer and apply the correct rate
+if accountYear >= 2020:
+  dataUsageSubtotal = dataUsage * NEW_CUSTOMER_RATE
+else:
+  dataUsageSubtotal = dataUsage * OLD_CUSTOMER_RATE
+
+# Check if the data usage is above the limit to correct the charge
+if dataUsageSubtotal > 30:
+  dataUsageSubtotal = 30.00
+
+# Sum the data usage with the default value for calls and texts
+total = dataUsageSubtotal + UNLIMITED_CALLS_AND_TEXTS;
+
+# Print all results
+print('Calls and Texts: ' + formatCurrency(UNLIMITED_CALLS_AND_TEXTS))
+print('Data: ' + formatCurrency(dataUsageSubtotal))
+print('Total: ' + formatCurrency(total))
