84c1214dc38cde96bc35bf9ef4ce706090a748c2
Matheus Carvalho <matheus12bh@gmail.com>
2022-09-21 15:56:01 -0700

    add-loops

--- Individual Assignment 3/individual_assignment_3.py
+++ Individual Assignment 3/individual_assignment_3.py
@@ -12,3 +12,19 @@ Processing:
 # Define constants to facilitate further updates
 
 print('Please enter the number of books checked out:')
+numberOfBooks = int(input())
+
+if numberOfBooks < 1:
+  print('The number of books checked out should be a positive number.\nPlease enter again:')
+
+bookNumber = 0
+
+while bookNumber < numberOfBooks:
+  bookNumber += 1
+  print('For book ' + str(bookNumber) + ' please enter the number of days overdue.')
+  int(input())
+  #Check value for this one
+  print('Book 1: 10 day(s) overdue, the fine is $1.3')
+
+  if bookNumber == numberOfBooks:
+    print('Calculate the fine for another patron? (Y/y) to continue')
