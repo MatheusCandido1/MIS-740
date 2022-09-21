15584411217fccfdc5e7d3c50a8325910ec0037e
Matheus Carvalho <matheus12bh@gmail.com>
2022-09-21 15:34:39 -0700

    add-individual-assignment-3-files

---
+++ Individual Assignment 3/individual_assignment_3.py
@@ -0,0 +1,14 @@
+"""
+# Fine Calculator for Overdue Books
+
+Input: 
+Output: 
+Processing: 
+1 - Get user input
+
+* Author: Matheus Carvalho
+* Date: 09-21-2022
+"""
+# Define constants to facilitate further updates
+
+print('Please enter the number of books checked out:')
