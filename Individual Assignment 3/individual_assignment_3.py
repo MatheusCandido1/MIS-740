d9305d9f7e50351df51d24d26f86e950f601de02
Matheus Carvalho <matheus12bh@gmail.com>
2022-09-28 12:45:57 -0700

    finish-individual-assignment-3

--- Individual Assignment 3/individual_assignment_3.py
+++ Individual Assignment 3/individual_assignment_3.py
@@ -7,24 +7,61 @@ Processing:
 1 - Get user input
 
 * Author: Matheus Carvalho
-* Date: 09-21-2022
+* Date: 09-28-2022
 """
-# Define constants to facilitate further updates
+REGULAR_FINE_RATE = 0.10
+ADDITIONAL_FINE_RATE = 0.20
+MAXIMUM_DAYS_TO_GET_REGULAR_FINE_RATE = 7
 
-print('Please enter the number of books checked out:')
-numberOfBooks = int(input())
+def formatToCurrency(value):
+  return '$' + str(format(value,'.2f'))
+  
+def get_regular_fine(days):
+    return (days) * REGULAR_FINE_RATE
 
-while numberOfBooks < 1:
-  print('The number of books checked out should be a positive number.\nPlease enter again:')
+def get_additional_fine(days):
+  return (days - 7) * ADDITIONAL_FINE_RATE
 
-bookNumber = 0
+def get_fine_by_amount_of_days_overdue(days):
+  if days <= MAXIMUM_DAYS_TO_GET_REGULAR_FINE_RATE:
+    return get_regular_fine(days)
+  else:
+    return get_regular_fine(MAXIMUM_DAYS_TO_GET_REGULAR_FINE_RATE) + get_additional_fine(days)
+  
 
-while bookNumber < numberOfBooks:
-  bookNumber += 1
-  print('For book ' + str(bookNumber) + ' please enter the number of days overdue.')
-  int(input())
-  #Check value for this one
-  print('Book 1: 10 day(s) overdue, the fine is $1.3')
+calculateFine = True
 
-  if bookNumber == numberOfBooks:
-    print('Calculate the fine for another patron? (Y/y) to continue')
+while calculateFine:
+  totalFine = 0.00
+  print('Please enter the number of books checked out:')
+  numberOfBooks = int(input())
+
+  while numberOfBooks < 1:
+    print('The number of books checked out should be a positive number.\nPlease enter again:')
+    numberOfBooks = int(input())
+
+  bookNumber = 0
+
+  while bookNumber < numberOfBooks:
+    bookNumber += 1
+    print('For book ' + str(bookNumber) + ', please enter the number of days overdue.')
+    numberOfDaysOverdue = int(input())
+
+    while numberOfDaysOverdue < 0:
+      print('The number of days overdue cannot be negative number.\nPlease enter again:')
+      numberOfDaysOverdue = int(input())
+    #Check value for this one
+    fineByBook = get_fine_by_amount_of_days_overdue(numberOfDaysOverdue)
+    totalFine += float(fineByBook)
+
+    print('Book ' + str(bookNumber) + ': ' + str(numberOfDaysOverdue) + ' day(s) overdue, the fine is ' + str(formatToCurrency(fineByBook)))
+    if bookNumber == numberOfBooks:
+      print('The total fine for the patron is: ' + formatToCurrency(totalFine))
+
+  print('Calculate the fine for another patron? (Y/y) to continue')
+  calculateFineAgain = input()
+
+  if calculateFineAgain.lower() == 'y':
+    calculateFine = True
+  else:
+    calculateFine = False
