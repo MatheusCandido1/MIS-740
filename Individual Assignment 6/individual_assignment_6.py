ce4bcfb1aa4ba5c4cd7f2171bf3093466a164732
Matheus Carvalho <matheus12bh@gmail.com>
2022-10-27 12:44:58 -0700

    add-prints

---
+++ Individual Assignment 6/individual_assignment_6.py
@@ -0,0 +1,26 @@
+"""
+# Reward Points Calculation
+
+"""
+
+utahCustomersSpendingRecords = 0
+nevadaCustomersSpendingRecords = 0
+
+print('There are ' + str(utahCustomersSpendingRecords) + ' records for Utah customers.')
+print('There are ' + str(nevadaCustomersSpendingRecords) + ' records for Nevada customers. \n')
+
+mean = 0
+std = 0
+median = 0
+
+print('The mean of points earnded is: ' + str(mean))
+print('The standard deviation of the of points earnded is: '+ str(std))
+print('The median of points earnded is: '+ str(median) + '\n')
+
+planitumCustomers = 0
+goldCustomers = 0
+siilverCustomers = 0
+
+print('Platinum customers: ' + str(planitumCustomers))
+print('Gold customers: ' + str(goldCustomers))
+print('Silver customers: ' + str(siilverCustomers))
